0be6bac942f2511ed8b1d59d089ace8a2f5757e3
yukuai <1635610645@qq.com>
2025-04-10 12:07:58 +0800

    初始化仓库

---
+++ WebApiTemplate/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+
+namespace WebApiTemplate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string baseAddress = "http://localhost:8079/";
+            using (WebApp.Start<Startup>(baseAddress))
+            {
+                Console.WriteLine("Server running at {0}", baseAddress);
+                Console.ReadLine(); // Keep the console application running
+            }
+        }
+    }
+}
---
+++ WebApiTemplate/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using System.Web.Http;
+[assembly: OwinStartup(typeof(WebApiTemplate.Startup))]
+namespace WebApiTemplate
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // 配置应用程序管道
+            app.UseCors(CorsOptions.AllowAll);
+
+            HttpConfiguration config = new HttpConfiguration();
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            app.UseWebApi(config);
+
+            app.Map("/send", appcur => appcur.Use((ctx, next) =>
+            {
+                return ctx.Response.WriteAsync("ok");
+            }));
+        }
+    }
+}
